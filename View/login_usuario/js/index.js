0254f73b6c6eb55963915fdc06dc53ee02f803d6
Paulo José (d3xt3er) <48564999+d3xt3er@users.noreply.github.com>
2021-06-22 11:41:51 -0300

    Index.js
    Autenticação de login funcionando

--- View/login_usuario/js/index.js
+++ View/login_usuario/js/index.js
@@ -1,22 +1,21 @@
 $(document).ready(function() {
-    $("form").submit(function(event) {
-        var formData = {
-            nome: $("#nome").val(),
-            senha: $("#senha").val(),
-        };
-        console.log(formData);
-        $.ajax({
-            type: "GET",
-            url: "https://backend-recyclo.herokuapp.com/usuario/login/",
-            data: formData,
-            dataType: "json",
-            encode: true,
-        }).then(function(data) {
-            alert(data);
-        }).catch(function(error) {
-            alert(error.responseText);
-        });
+    $("#form").submit(function(event) {
 
         event.preventDefault();
+
+        var nome = document.getElementById("nome").value;
+        var senha = document.getElementById("senha").value;
+
+        var xhr = new XMLHttpRequest();
+        var url = "http://localhost:8080/usuario/login/" + nome + "/" + senha + "";
+        xhr.open("GET", url, true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var json = xhr.responseText;
+                alert(json);
+            }
+        };
+        xhr.send();
     });
 });
