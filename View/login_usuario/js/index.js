d8930cb30347784ed2abc0b17aab4758e11a5349
Paulo José (d3xt3er) <48564999+d3xt3er@users.noreply.github.com>
2021-06-30 16:30:17 -0300

    Cadastro
    Mascara CPF, CNPJ aplicada

--- View/login_usuario/js/index.js
+++ View/login_usuario/js/index.js
@@ -6,27 +6,27 @@ $(document).ready(function() {
         var nome = document.getElementById("nome").value;
         var senha = document.getElementById("senha").value;
 
-        var xhr = new XMLHttpRequest();
+        var request = new XMLHttpRequest();
         var url = "https://backend-recyclo.herokuapp.com/usuario/login/" + nome + "/" + senha + "";
-        xhr.open("GET", url, true);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                var json = xhr.responseText;
+        request.open("GET", url, true);
+        request.setRequestHeader("Content-Type", "application/json");
+        request.setRequestHeader('Access-Control-Allow-Origin', '*');
+        request.onreadystatechange = function() {
+            if (request.readyState === 4 && request.status === 200) {
+                var json = request.responseText;
                 alert(json);
             }
         };
-        xhr.send();
+        request.send();
     });
 
 
-/* mensagem de erro padrao sweetalert
-    Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Something went wrong!',
-      })
-*/
+    /* mensagem de erro padrao sweetalert
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Something went wrong!',
+          })
+    */
 
 });
-
