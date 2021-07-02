e8860e6dff11953ac7650fd9037bc615886f890d
Paulo José (d3xt3er) <48564999+d3xt3er@users.noreply.github.com>
2021-07-02 16:20:13 -0300

    Update
    Cadastro de empresa

--- View/login_usuario/js/index.js
+++ View/login_usuario/js/index.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
     $("#form").submit(function(event) {
-
+        Swal.fire({
+            title: 'Aguarde',
+            html: 'Verificando...',
+            allowOutsideClick: false,
+            onBeforeOpen: () => {
+                Swal.showLoading()
+            },
+        });
         event.preventDefault();
 
         var nome = document.getElementById("nome").value;
@@ -11,20 +18,15 @@ $(document).ready(function() {
         request.open("GET", url, true);
         request.setRequestHeader("Content-Type", "application/json");
         request.setRequestHeader('Access-Control-Allow-Origin', '*');
+      
         request.onreadystatechange = function() {
-
-            // Função loader sweetalert
-            Swal.fire({
-                title: 'Aguarde!',
-                html: 'Verificando...',
-                allowOutsideClick: false,
-                onBeforeOpen: () => {
-                    Swal.showLoading()
-                },
-            });
+          
             if (request.readyState === 4 && request.status === 200) {
-                var json = request.responseText;
+                var json = (request.responseText);
+               
                 if(json == 'Usuario encontrado!'){
+                 // Função loader sweetalert
+                
                     alert(json)
                 }
 
