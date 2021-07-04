e16279af11f9966472a4631825d10361878a5eaf
Caio <caiodjesus1@hotmail.com>
2021-07-04 14:38:40 -0300

    Update
    Padronização do login e alterações na home

--- View/mapa/index.js
+++ View/mapa/index.js
@@ -1,5 +1,27 @@
+/* proximos passos do mapa
+* -infobox
+* -icones
+*/ 
+
+function GetMap() {
+    var map = new Microsoft.Maps.Map('#myMap');
 
 
-function loadmap(){
-    var map = new Microsoft.Maps.Map('#map');
+    var center = map.getCenter();
+
+    //Create custom Pushpin
+    var pin = new Microsoft.Maps.Pushpin(center, {
+        icon: 'LogoRecycloV1-mapa-icon.png',
+        anchor: new Microsoft.Maps.Point(12, 39)
+    });
+
+    //Add the pushpin to the map
+    map.entities.push(pin);
+    //Add your post map load code here.
+}
+
+
+
+function alert(){
+
 }
