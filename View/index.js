f0ed26df970cc03b2779cf29cbf19b59c41d4411
Caio <caiodjesus1@hotmail.com>
2021-07-07 18:56:57 -0300

    Update
    Atualização da home

---
+++ View/index.js
@@ -0,0 +1,15 @@
+
+function goMapa(){
+    window.location.assign("mapa/index.html");
+}
+
+function goSobre(){
+    window.location.assign("Sobre/index.html");
+}
+
+function gologin(){
+    window.location.assign("login_usuario/index.html");
+}
+function goCadastro(){
+    window.location.assign("Cadastro/index.html");
+}
