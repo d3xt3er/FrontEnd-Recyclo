f2c861270bc02e443fcd17f0cd7614a31dbd3d81
Paulo José (d3xt3er) <48564999+d3xt3er@users.noreply.github.com>
2021-07-14 17:39:49 -0300

    Front
    Alteração e adição de conteúdo, e imagens, Fonte de texto alterada

--- View/mapa/index.js
+++ View/mapa/index.js
@@ -3,6 +3,33 @@
  * -icones
  */
 
+function Logout() {
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+    })
+
+    swalWithBootstrapButtons.fire({
+        title: 'Tem certeza que deseja sair?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Sim',
+        cancelButtonText: 'Não',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.replace("../login_usuario/index.html");
+            window.localStorage.removeItem('nome');
+            window.localStorage.removeItem('senha');
+
+        }
+    })
+}
+
+
 function GetMap() {
     var map = new Microsoft.Maps.Map('#myMap');
 
