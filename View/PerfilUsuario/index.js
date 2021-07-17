a5191d23f2f19ee999eedda86925d826a64613c8
Paulo José (d3xt3er) <48564999+d3xt3er@users.noreply.github.com>
2021-07-17 10:38:28 -0300

    Front
    Plotando informações da API no perfil usuario, ajustando css front

--- View/PerfilUsuario/index.js
+++ View/PerfilUsuario/index.js
@@ -1,40 +1,65 @@
-
-
-function alterarConta(){
+function alterarConta() {
     Swal.fire({
-        title:"Editar informações",
-        html:'<form id="EditarInformacao">'+
-        '<input id="FormNome" placeholder="Nome:" class="txtEditar" type="text">'+
-        '<input id="formCpf" placeholder="Cpf:"class="txtEditar" type="text">'+
-        '<input id="formTelefone" placeholder="Telefone:" class="txtEditar" type="text">'+
-        '<input id="formSenha" placeholder="Senha:" type="password"class="txtEditar" type="text">'+
-        '<input id="formConfirmar"placeholder="Confirmar nova senha:" type="password" class="txtEditar" type="text">'+
-        '</form>',
+        title: "Editar informações",
+        html: '<form id="EditarInformacao">' +
+            '<input id="FormNome" placeholder="Nome:" class="txtEditar" type="text">' +
+            '<input id="formCpf" placeholder="CPF:"class="txtEditar" type="text">' +
+            '<input id="formTelefone" placeholder="Telefone:" class="txtEditar" type="text">' +
+            '<input id="formSenha" placeholder="Senha:" type="password"class="txtEditar" type="text">' +
+            '<input id="formConfirmar"placeholder="Confirmar nova senha:" type="password" class="txtEditar" type="text">' +
+            '</form>',
         showDenyButton: true,
         showCancelButton: true,
-        confirmButtonText: `Save`,
+        confirmButtonText: `Salvar`,
         denyButtonText: `Don't save`,
     });
 }
 
-function excluirConta(){
+function excluirConta() {
     Swal.fire({
         text: "Você realmente deseja deletar esta conta?",
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
-        cancelButtonText:'Não',
+        cancelButtonText: 'Não',
         confirmButtonText: 'Sim'
-      }).then((result) => {
+    }).then((result) => {
         if (result.isConfirmed) {
             //funcao deletar
-          Swal.fire(
-            'Conta excluida com sucesso',
-            '',
-            'success'
-          )
+            Swal.fire(
+                'Conta excluida com sucesso',
+                '',
+                'success'
+            )
         }
-      })
+    })
 }
 
+
+
+
+var nome = localStorage.getItem('nome');
+var senha = localStorage.getItem('senha');
+
+fetch(`https://backend-recyclo.herokuapp.com/usuario/user/${nome}/${senha}`, {
+        method: 'get'
+    })
+    .then((resp) => resp.json())
+    .then(function(data) {
+
+        // Nome do localstorage
+        var name = localStorage.getItem('nome');
+        document.getElementById("title").innerHTML = name;
+
+        // informações vindas da API
+        document.getElementById("id_usuario").innerHTML = data.cd_usuario;
+        document.getElementById("nome").innerHTML = data.nm_usuario;
+        document.getElementById("cpf").innerHTML = data.cd_cpf;
+        document.getElementById("email").innerHTML = data.ds_email;
+        // document.getElementById("senha").innerHTML = data.cd_senha;
+
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
--- View/mapa/index.js
+++ View/mapa/index.js
@@ -31,42 +31,14 @@ function Logout() {
 }
 
 function GetMap() {
-    // $(document).ready(() => {
-    //     $("body").load(() => {
+
     var map = new Microsoft.Maps.Map('#myMap');
 
-    navigator.geolocation.getCurrentPosition(function(position){
+    navigator.geolocation.getCurrentPosition(function(position) {
         var loc = new Microsoft.Maps.Location(
             position.coords.latitude,
             position.coords.longitude);
 
-        map.setView({center:loc,zoom:17});
+        map.setView({ center: loc, zoom: 15 });
     });
 }
-
-
-
-
-
-var nome = localStorage.getItem('nome');
-var senha = localStorage.getItem('senha');
-
-fetch(`https://backend-recyclo.herokuapp.com/usuario/user/${nome}/${senha}`, {
-        method: 'get'
-    })
-    .then((resp) => resp.json())
-    .then(function(data) {
-
-        // Nome do localstorage
-        // var x = localStorage.getItem('nome');
-        // document.getElementById("usuario").innerHTML = x;
-
-        // informações vindas da API
-        // document.getElementById("cpf").innerHTML = data.cd_cpf;
-        // document.getElementById("senha").innerHTML = data.cd_senha;
-        // document.getElementById("email").innerHTML = data.ds_email;
-
-    })
-    .catch(function(err) {
-        console.log(err);
-    });
