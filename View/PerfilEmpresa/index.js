9b5602e17e3466284e4c7d2ed4bd80c1238b0413
Paulo José (d3xt3er) <48564999+d3xt3er@users.noreply.github.com>
2021-08-13 19:21:21 -0300

    Update index.js
    Crud completo ponto de coleta

--- View/PerfilEmpresa/index.js
+++ View/PerfilEmpresa/index.js
@@ -5,10 +5,10 @@ var nome = localStorage.getItem('nome');
 var senha = localStorage.getItem('senha');
 
 // VARIAVEIS PARA O FORMULARIO DE EDIÇÃO
+var id_company;
 var nameCompany;
 var cnpjCompany;
 var telefoneCompany;
-
 
 fetch(`https://backend-recyclo.herokuapp.com/empresa/company/${nome}/${senha}`, {
         method: 'get'
@@ -54,13 +54,14 @@ getPoints().then(res => {
 
         const linome = document.createElement('li');
         const txt = document.createElement('a');
-        txt.innerText = point.nmPonto;
+        // const nm = document.createElement('p');
+        var nome_ponto = txt.innerText = point.nm_ponto;
         linome.appendChild(txt);
         ul.appendChild(linome);
 
         const logra = document.createElement('li');
-        logra.id = `${point.cd_ponto_coleta}`;
-        txt.innerText = point.nm_logradouro;
+        var id_ponto = logra.id = `${point.cd_ponto_coleta}`;
+        var endereco_ponto = txt.innerText = point.nm_logradouro;
         logra.appendChild(txt);
         ul.appendChild(logra);
 
@@ -75,10 +76,119 @@ getPoints().then(res => {
             if (i == 0) { // Deletar
                 btn.src = "../img/icones/deletar.png";
                 btn.title = "Deletar Ponto";
+                btn.onclick = function() {
+                    var cod_empresa = document.getElementById("id_empresa").textContent;
+                    Swal.fire({
+                        text: "Você realmente deseja deletar esta ponto de coleta?",
+                        icon: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        cancelButtonText: 'Não',
+                        confirmButtonText: 'Sim'
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+
+                            var dPoint = JSON.stringify({
+                                "id": id_ponto,
+                                "id_empresa": cod_empresa
+                            })
+
+                            // var url = "http://localhost:8080/empresa/deletar/ponto/";
+
+                            var url = "https://backend-recyclo.herokuapp.com/empresa/deletar/ponto/";
+
+                            var request = new XMLHttpRequest();
+                            request.open("DELETE", url);
+
+                            request.setRequestHeader("Accept", "application/json");
+                            request.setRequestHeader("Content-Type", "application/json");
+
+                            request.onreadystatechange = function() {
+                                if (request.readyState === 4) {
+                                    console.log(request.status);
+                                    console.log(request.responseText);
+                                }
+                            };
+
+                            request.send(dPoint);
+
+                            Swal.fire({
+                                icon: 'success',
+                                title: 'Deletado com sucesso!',
+                                showConfirmButton: false,
+                                timer: 1500
+                            }).then((result) => {
+                                if (result.dismiss === Swal.DismissReason.timer) {
+                                    location.reload()
+                                }
+                            })
+                        }
+                    })
+
+                }
                 li.appendChild(btn);
             } else { // editar
                 btn.src = "../img/icones/editar.png";
                 btn.title = "Editar Ponto";
+                btn.onclick = function() {
+
+
+                    Swal.fire({
+                        title: "Adicionar Ponto de Coleta",
+                        html: '<form id="EditarInformacao">' +
+                            `<input id="name_ponto" placeholder="Nome do Ponto:" class="txtEditar" type="text" autocomplete="off" value="${nome_ponto}">` +
+                            `<input id="pontoE" placeholder="Endereço do Ponto:" class="txtEditar" type="text" autocomplete="off" value="${endereco_ponto}">` +
+                            '</form>',
+                        showDenyButton: true,
+                        showCancelButton: false,
+                        confirmButtonText: `Adicionar`,
+                        denyButtonText: `Cancelar`,
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+
+                            var nomeP = document.getElementById("name_ponto").value;
+                            var PontoE = document.getElementById("pontoE").value;
+
+                            // var url = "http://localhost:8080/empresa/ponto/alterar";
+                            var url = "https://backend-recyclo.herokuapp.com/empresa/ponto/alterar";
+
+                            var aPoint = JSON.stringify({
+                                "id": id_ponto,
+                                "nome": nomeP,
+                                "logradouro": PontoE
+                            })
+
+                            // console.log(aPoint);
+                            var xhr = new XMLHttpRequest();
+                            xhr.open("PUT", url);
+
+                            xhr.setRequestHeader("Accept", "application/json");
+                            xhr.setRequestHeader("Content-Type", "application/json");
+
+                            xhr.onreadystatechange = function() {
+                                if (xhr.readyState === 4) {
+                                    console.log(xhr.status);
+                                    console.log(xhr.responseText);
+                                }
+                            };
+
+                            xhr.send(aPoint);
+
+                            Swal.fire({
+                                icon: 'success',
+                                title: 'Alterado com sucesso!',
+                                showConfirmButton: false,
+                                timer: 1500
+                            }).then((result) => {
+                                if (result.dismiss === Swal.DismissReason.timer) {
+                                    location.reload()
+                                }
+                            })
+                        }
+                    })
+
+                }
                 li.appendChild(btn);
             }
             ul.appendChild(li);
@@ -89,7 +199,7 @@ getPoints().then(res => {
 
 });
 
-
+//ALTERAR - Empresa
 function EditarInfo() {
 
     Swal.fire({
@@ -113,7 +223,7 @@ function EditarInfo() {
     }).then((result) => {
         if (result.isConfirmed) {
 
-            //ALTERAR - Empresa
+
             var id = document.getElementById("id_empresa").textContent;
             var nome = document.getElementById("FormNome").value;
             var telefone = document.getElementById("formTelefone").value;
@@ -146,7 +256,6 @@ function EditarInfo() {
             request.send(company);
 
             Swal.fire({
-
                 icon: 'success',
                 title: 'Alterado com sucesso!',
                 showConfirmButton: false,
@@ -170,25 +279,25 @@ function hiddenPassword() {
 }
 
 // deletar Ponto de coleta
-function deletarPonto() {
-    Swal.fire({
-        text: "Você realmente deseja deletar esta ponto de coleta?",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        cancelButtonText: 'Não',
-        confirmButtonText: 'Sim'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            Swal.fire(
-                'Ponto Deletado',
-                '',
-                'success'
-            )
-        }
-    })
-}
+// function deletarPonto() {
+//     Swal.fire({
+//         text: "Você realmente deseja deletar esta ponto de coleta?",
+//         icon: 'warning',
+//         showCancelButton: true,
+//         confirmButtonColor: '#3085d6',
+//         cancelButtonColor: '#d33',
+//         cancelButtonText: 'Não',
+//         confirmButtonText: 'Sim'
+//     }).then((result) => {
+//         if (result.isConfirmed) {
+//             Swal.fire(
+//                 'Ponto Deletado',
+//                 '',
+//                 'success'
+//             )
+//         }
+//     })
+// }
 
 // Função adicionar ponto
 function adicionarPonto() {
@@ -239,9 +348,8 @@ function adicionarPonto() {
                 });
 
             Swal.fire({
-
                 icon: 'success',
-                title: 'Alterado com sucesso!',
+                title: 'Adicionado com sucesso!',
                 showConfirmButton: false,
                 timer: 1500
             }).then((result) => {
@@ -253,16 +361,16 @@ function adicionarPonto() {
     })
 }
 
-function editarPonto() {
-    Swal.fire({
-        title: "Adicionar Ponto de Coleta",
-        html: '<form id="EditarInformacao">' +
-            '<input id="nmPonto" placeholder="Nome do Ponto:" class="txtEditar" type="text">' +
-            '<input id="pontoTelefone" placeholder="telefone do Ponto:" class="txtEditar" type="text">' +
-            '</form>',
-        showDenyButton: true,
-        showCancelButton: false,
-        confirmButtonText: `Adicionar`,
-        denyButtonText: `Cancelar`,
-    });
-}
+// function editarPonto() {
+//     Swal.fire({
+//         title: "Adicionar Ponto de Coleta",
+//         html: '<form id="EditarInformacao">' +
+//             '<input id="nmPonto" placeholder="Nome do Ponto:" class="txtEditar" type="text">' +
+//             '<input id="pontoTelefone" placeholder="telefone do Ponto:" class="txtEditar" type="text">' +
+//             '</form>',
+//         showDenyButton: true,
+//         showCancelButton: false,
+//         confirmButtonText: `Adicionar`,
+//         denyButtonText: `Cancelar`,
+//     });
+// }
